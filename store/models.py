acc206760f2988560114346206cafd2b5a150e20
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-04-25 10:14:24 +0530

    Running Migrations

--- store/models.py
+++ store/models.py
@@ -16,8 +16,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     last_update = models.DateTimeField(auto_now=True)
 
     ###
@@ -43,6 +44,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=50)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
 
 class Order(models.Model):
 
@@ -82,8 +84,7 @@ class CartItem(models.Model):
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-
+    city = models.CharField(max_length=255)    
     ###
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
