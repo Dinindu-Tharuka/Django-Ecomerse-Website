6069c5c4e6717cc17346371fdb9c169b9e5acb7a
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-04-26 10:16:53 +0530

    Install Django Debug toolbar

--- storefront/settings.py
+++ storefront/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'playground',
     'store',
     'likes',
-    'tag'
+    'tag',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -51,7 +52,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
 
 ROOT_URLCONF = 'storefront.urls'
 
