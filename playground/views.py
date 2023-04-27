913850981c86d75824832ab434e1edea550dc1c4
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-04-27 19:12:07 +0530

    select related andperfetch related consept

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 def say_hello(request):
 
-    orderitem_set = OrderItem.objects.values('product__title').distinct().order_by('product__title')
-
-    # query_set = OrderItem.objects.values('product_id').distinct()
-
-    # query_set_product = Product.objects.filter(id__in=query_set)
+    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set')\
+                .order_by('place_at')[:5]
     
-    count_product = orderitem_set.count()
+
+    count = query_set.count()
+    
 
 
-    return render(request, 'hello.html', {'products' : list(orderitem_set), 'count':count_product})
+    return render(request, 'hello.html', {'products' : list(query_set), 'count':count})
