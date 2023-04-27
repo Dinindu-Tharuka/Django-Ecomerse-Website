a069ef307de2bb46b699644571d3bf98fc654969
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-04-27 16:24:30 +0530

    Limiting output

--- playground/views.py
+++ playground/views.py
@@ -5,7 +5,7 @@ from store.models import Product
 
 def say_hello(request):
 
-    product = Product.objects.filter(Q(unit_price__gt=3000) | Q(unit_price__lt=4000))
+    product = Product.objects.filter(Q(unit_price__gt=3000) | Q(unit_price__lt=4000))[:50]
     count_product = product.count()
 
     return render(request, 'hello.html', {'products' : list(product), 'count':count_product})
