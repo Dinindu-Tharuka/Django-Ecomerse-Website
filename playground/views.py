edaa7be095402f01c5eebf04987df6da7351c61c
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-04-27 16:23:45 +0530

    Q Objects

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
+from store.models import Product
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name' : 'Dinindu'})
+
+    product = Product.objects.filter(Q(unit_price__gt=3000) | Q(unit_price__lt=4000))
+    count_product = product.count()
+
+    return render(request, 'hello.html', {'products' : list(product), 'count':count_product})
