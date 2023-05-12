21d522386bc46ff08d50dec28ea4e852b72e3c8b
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-05-12 11:00:58 +0530

    Add Model View set Option to the project

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
 
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:id>', views.ProductDetails.as_view()),
-    path('collection/', views.CollectionList.as_view()),
-    path('collection/<int:id>/', views.CollectionDetails.as_view())
-]
+urlpatterns = router.urls
+# [
+#     path('products/', views.ProductList.as_view()),
+#     path('products/<int:pk>/', views.ProductDetails.as_view()),
+#     path('collection/', views.CollectionList.as_view()),
+#     path('collection/<int:pk>/', views.CollectionDetails.as_view())
+# ]
--- store/views.py
+++ store/views.py
@@ -5,66 +5,44 @@ from rest_framework.decorators import api_view
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from .serializers import ProductSerializer, CollectionSerializer
-from .models import Product, Collection
+from .models import Product, Collection, OrderItem
 
 ######### Product ############
 
-class ProductList(ListCreateAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer   
-    
-class ProductDetails(APIView):   
-    
-    def get(self, request, id):     
-        product = get_object_or_404(Product, pk=id)          
-        seralizer = ProductSerializer(product)
-        return Response(seralizer.data)
-    
-    def put(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product, request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-    
-    def delete(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        if product.orderitem_set.count() > 0:
+    serializer_class = ProductSerializer 
+
+    def destroy(self, request, *args, **kwargs):
+        
+        if OrderItem.objects.filter(product__id=kwargs['pk']).count() > 0:
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        return super().destroy(request, *args, **kwargs)  
+
+     
     
     
 ############## Collection End Point ###########
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(product_count=Count('product')).all()
     serializer_class = CollectionSerializer 
 
+    def destroy(self, request, *args, **kwargs):
 
-class CollectionDetails(APIView):
-
-    def get(self, request, id):
-        queryset = Collection.objects.annotate(product_count=Count('product')).all()    
-        collection = get_object_or_404(queryset, pk=id)
-        serializer = CollectionSerializer(collection)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def put(self, request, id):
-        collection = Collection.objects.get(pk=id)
-        serializer = CollectionSerializer(collection, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-    
-    def delete(self, request, id):
-        collection = Collection.objects.get(pk=id)
-        if collection.product_set.count() > 0:
+        if Product.objects.filter(collection__id=kwargs['pk']).count() > 0:
             return Response({'error': "You can't delete this collection, because this collection has some products."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+
+    
+
+
+    
+    
+
 
 
 
