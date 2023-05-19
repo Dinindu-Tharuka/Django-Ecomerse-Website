b57f174aa1956dee878418f236ed9642358b9a0f
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-05-19 07:56:53 +0530

    Add Pagination and create custom pagination class

--- store/views.py
+++ store/views.py
@@ -2,13 +2,15 @@ from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from .models import Product, Collection, OrderItem, Reviews
 from .filters import ProductPriceFilter
+from .pagination import DefaultPagination
 
 
 ######### Product ############
@@ -16,10 +18,11 @@ from .filters import ProductPriceFilter
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer 
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductPriceFilter
     search_fields = ['title', 'description']
-    
+    ordering_fields = ['unit_price', 'last_update']
+    pagination_class = DefaultPagination
 
     # def get_queryset(self):
     #     queryset = Product.objects.all()
